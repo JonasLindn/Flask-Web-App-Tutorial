2ef49087086ebd1da237a128b0d0c6a6e8afa3d8
lindner71 <jonas.lindner@uni-potsdam.de>
2025-01-05 20:11:14 +0100

    frontend (fast fertig)

--- website/auth.py
+++ website/auth.py
@@ -17,13 +17,13 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Anmeldung erfolgreich!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Falsches Passwort, bitte erneut versuchen.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Diese Email existiert nicht.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -33,6 +33,30 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
+
+
+@auth.route('/new_project', methods=['GET', 'POST'])
+@login_required
+def new_project():
+    return render_template("new_project.html", user=current_user)
+
+
+@auth.route('/project_info', methods=['GET', 'POST'])
+@login_required
+def project_info():
+    return render_template("project_info.html", user=current_user)
+
+
+@auth.route('/rahmenbedingungen', methods=['GET', 'POST'])
+@login_required
+def rahmenbedingungen():
+    return render_template("rahmenbedingungen.html", user=current_user)
+
+
+@auth.route('/priority', methods=['GET', 'POST'])
+@login_required
+def priority():
+    return render_template("priority.html", user=current_user)
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -45,22 +69,22 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists.', category='error')
+            flash('Diese Email existiert bereits.', category='error')
         elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
+            flash('Email muss länger als 3 Zeichen sein.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('Vorname muss länger als 1 Zeichen sein.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+            flash('Passwörter stimmen nicht überein.', category='error')
         elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+            flash('Das Passwort muss mindestens eine länge von 7 Zeichen besitzen.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Account erstellt!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -24,6 +24,30 @@ def home():
     return render_template("home.html", user=current_user)
 
 
+@views.route('/new_project', methods=['GET', 'POST'])
+@login_required
+def new_project():
+    return render_template("new_project.html", user=current_user)
+
+
+@views.route('/project_info', methods=['GET', 'POST'])
+@login_required
+def project_info():
+    return render_template("project_info.html", user=current_user)
+
+
+@views.route('/rahmenbedingungen', methods=['GET', 'POST'])
+@login_required
+def rahmenbedingungen():
+    return render_template("rahmenbedingungen.html", user=current_user)
+
+
+@views.route('/priority', methods=['GET', 'POST'])
+@login_required
+def priority():
+    return render_template("priority.html", user=current_user)
+
+
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
